b370f782ff1929074a3eb574521db7810651aab6
Marco Bustamante <marco.bustam@gmail.com>
2018-10-24 22:04:01 -0300

    site.js

--- Automov1/wwwroot/js/site.js
+++ Automov1/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(document).ready(function () {
+    console.log("ready!");
+    $("#navbardrop").mouseover(function () {
+        console.log("<div>Handler for .mouseover() called.</div>");
+        $("#navbardrop").click();
+    });
+});
